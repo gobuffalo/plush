53104371852f73a5211a8cdb02894330fcd018b9
Mark Bates <mark@markbates.com>
2017-03-01 17:03:41 -0500

    grrr missed a code climate issue

--- token/token.go
+++ token/token.go
@@ -21,7 +21,7 @@ var keywords = map[string]Type{
 	"in":     IN,
 }
 
-// Lookup an ident and return a keyword type, or a plain ident
+// LookupIdent an ident and return a keyword type, or a plain ident
 func LookupIdent(ident string) Type {
 	if tok, ok := keywords[ident]; ok {
 		return tok

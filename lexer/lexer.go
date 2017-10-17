cfc836e5b60b8fa17e5ed3d547bee48b817ef9ae
Todd Rafferty <webRat@gmail.com>
2017-10-17 14:43:41 -0400

    Removed MustCompile / Regex

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -196,8 +196,6 @@ func (l *Lexer) nextInsideToken() token.Token {
 	return tok
 }
 
-var floatX = regexp.MustCompile(`\d*\.\d*`)
-
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()

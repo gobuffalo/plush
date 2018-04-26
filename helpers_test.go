eb827fe5817ba1e8ac75b6f9476f8a08373f697f
Mark Bates <mark@markbates.com>
2018-04-26 17:45:58 -0400

    improved helper args (#48)
    * wip
    
    * removed Export to export

--- helpers_test.go
+++ helpers_test.go
@@ -7,6 +7,36 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func Test_Helpers_WithoutData(t *testing.T) {
+	type data map[string]interface{}
+	r := require.New(t)
+
+	table := []struct {
+		I string
+		E string
+	}{
+		{I: `<%= foo() {return bar + name} %>`, E: "BARunknown"},
+		{I: `<%= foo({name: "mark"}) {return bar + name} %>`, E: "BARmark"},
+	}
+
+	for _, tt := range table {
+		ctx := NewContext()
+		ctx.Set("name", "unknown")
+		ctx.Set("bar", "BAR")
+		ctx.Set("foo", func(d data, help HelperContext) (string, error) {
+			c := help.New()
+			if n, ok := d["name"]; ok {
+				c.Set("name", n)
+			}
+			return help.BlockWith(c)
+		})
+		s, err := Render(tt.I, ctx)
+		r.NoError(err)
+		r.Equal(tt.E, s)
+	}
+
+}
+
 func Test_truncateHelper(t *testing.T) {
 	r := require.New(t)
 	x := randx.String(51)

b0b0a05f963a529b49d835f868fa1e2b92f4b144
Mat Ryer <matryer@users.noreply.github.com>
2018-06-09 20:03:44 +0100

    Added optional data map to contentFor/contentOf helpers (#55)
    * Added optional data map to contentFor/contentOf helpers
    
    addresses https://github.com/gobuffalo/plush/issues/54
    
    * added example to README
    
    * better docs
    
    * explained the map is optional
    
    * ensured map doesn't affect outer context
    
    * added better comment
    
    * better implementation of creating sub-block
    
    * defensive programming to avoid panic
    
    This is a freebee :)

--- content_helper_test.go
+++ content_helper_test.go
@@ -19,3 +19,21 @@ func Test_ContentForOf(t *testing.T) {
 	r.Contains(s, "<b1><button>hi</button></b1>")
 	r.Contains(s, "<b2><button>hi</button></b2>")
 }
+
+func Test_ContentForOfWithData(t *testing.T) {
+	r := require.New(t)
+	input := `
+	<b0><% contentFor("buttons") { %><button><%= label %></button><% } %></b0>
+	<b1><%= contentOf("buttons", {"label": "Button One"}) %></b1>
+	<b2><%= contentOf("buttons", {"label": "Button Two"}) %></b2>
+	<b3><%= label %></b3>
+	`
+	ctx := NewContext()
+	ctx.Set("label", "Outer label")
+	s, err := Render(input, ctx)
+	r.NoError(err)
+	r.Contains(s, "<b0></b0>")
+	r.Contains(s, "<b1><button>Button One</button></b1>")
+	r.Contains(s, "<b2><button>Button Two</button></b2>")
+	r.Contains(s, "<b3>Outer label</b3>", "the outer label shouldn't be affected by the map passed in")
+}

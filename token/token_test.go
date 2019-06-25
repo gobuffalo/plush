13ab0f306a11baf68da78b36c40fd86bd6bf825e
bart <bart.dy@gmail.com>
2019-06-25 09:35:12 +0200

    require token

--- token/token_test.go
+++ token/token_test.go
@@ -3,8 +3,8 @@ package token
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"github.com/gobuffalo/plush/token"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_Resolve_Default(t *testing.T) {

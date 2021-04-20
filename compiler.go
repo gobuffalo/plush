3f52817dcd2c15a35f0119b700ea8ba60ae7a986
mido <mido@numinix.com>
2021-04-20 14:33:34 -0700

    Revert "Improve evalIndexExpression based on code climate."
    This reverts commit 90f91c442888f87ed514933890b831f6151dafc8.

--- compiler.go
+++ compiler.go
@@ -280,55 +280,45 @@ func (c *compiler) evalIndexExpression(node *ast.IndexExpression) (interface{}, 
 	switch rv.Kind() {
 	case reflect.Map:
 		val := rv.MapIndex(reflect.ValueOf(index))
+		if !val.IsValid() && node.Value == nil {
+			return nil, nil
+		}
 
 		if node.Callee != nil {
 
-			rv = val
+			return c.evalIndexCallee(val, node)
 
-		} else {
-
-			if !val.IsValid() && node.Value == nil {
-				return nil, nil
-			}
-
-			if node.Value != nil {
-				rv.SetMapIndex(reflect.ValueOf(index), reflect.ValueOf(value))
-				return nil, nil
-			}
-
-			return val.Interface(), nil
 		}
+
+		if node.Value != nil {
+			rv.SetMapIndex(reflect.ValueOf(index), reflect.ValueOf(value))
+			return nil, nil
+		}
+
+		return val.Interface(), nil
 	case reflect.Array, reflect.Slice:
 		if i, ok := index.(int); ok {
 
 			if node.Callee != nil {
 
-				rv = rv.Index(i)
+				return c.evalIndexCallee(rv.Index(i), node)
 
-			} else {
-
-				if node.Value != nil {
-
-					if rv.Len()-1 < i {
-
-						return nil, fmt.Errorf("array index out of bounds, got index %d, while array size is %v", i, rv.Len())
-
-					}
-					rv.Index(i).Set(reflect.ValueOf(value))
-					return nil, nil
-				}
-				return rv.Index(i).Interface(), nil
 			}
+
+			if node.Value != nil {
+
+				if rv.Len()-1 < i {
+
+					return nil, fmt.Errorf("array index out of bounds, got index %d, while array size is %v", i, rv.Len())
+
+				}
+				rv.Index(i).Set(reflect.ValueOf(value))
+				return nil, nil
+			}
+			return rv.Index(i).Interface(), nil
 		}
 	}
-	var returnData interface{}
-	err = fmt.Errorf("could not index %T with %T", left, index)
-	if node.Callee != nil {
-
-		returnData, err = c.evalIndexCallee(rv, node)
-	}
-
-	return returnData, err
+	return nil, fmt.Errorf("could not index %T with %T", left, index)
 }
 func (c *compiler) evalHashLiteral(node *ast.HashLiteral) (interface{}, error) {
 	m := map[string]interface{}{}

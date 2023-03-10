dce8b2796eceeefd3ee0307013499282c9ff75b8
mido <mido@numinix.com>
2023-03-10 04:34:02 -0800

    Fix panic when accessing a map with a key type that's not comparable with map index

--- hashes_test.go
+++ hashes_test.go
@@ -6,6 +6,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func Test_Render_Hash_Key_Interface(t *testing.T) {
+	r := require.New(t)
+
+	input := `<%= m["first"]%>`
+	s, err := Render(input, NewContextWith(map[string]interface{}{
+
+		"m": map[interface{}]bool{"first": true},
+	}))
+	r.NoError(err)
+	r.Equal("true", s)
+}
+
+func Test_Render_Hash_Key_Int_With_String_Index(t *testing.T) {
+	r := require.New(t)
+
+	input := `<%= m["first"]%>`
+	_, err := Render(input, NewContextWith(map[string]interface{}{
+
+		"m": map[int]bool{0: true},
+	}))
+
+	errStr := "line 1: cannot use first (string constant) as int value in map index"
+	r.Error(err)
+	r.Equal(errStr, err.Error())
+
+}
+
 func Test_Render_Hash_Array_Index(t *testing.T) {
 	r := require.New(t)
 
